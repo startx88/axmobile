26e6794e6ca2501ef2ec2e85d29340e8d01b4d40
startx88 <arya.creativemind@gmail.com>
2022-01-05 22:25:14 +0530

    initial setup -3 added fonts and images

---
+++ utility/constants/SplashData.js
@@ -0,0 +1,8 @@
+export const Slides = [
+  {
+    id: 1,
+    title: 'Love your data.',
+    description: 'Transform your business with the power of data, analytics, and cloud.',
+    image: require('../../assets/images/home.png')
+  }
+]
